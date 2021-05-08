e5f59e75f97fb8ae5ab06f2252639fb48838377c
fred <fredyang@saas.top>
2021-05-08 15:42:16 +0800

    init--初始化一个maven项目

---
+++ yeyue-maven-club/src/main/java/club/yeyue/maven/YeyueMavenClubApplication.java
@@ -0,0 +1,13 @@
+package club.yeyue.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeyueMavenClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeyueMavenClubApplication.class, args);
+    }
+
+}
---
+++ yeyue-maven-club/src/test/java/club/yeyue/maven/YeyueMavenClubApplicationTests.java
@@ -0,0 +1,13 @@
+package club.yeyue.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeyueMavenClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
