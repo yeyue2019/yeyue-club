9527eabf462d0dcb45d1d8bed8d30ef2bb654621
fred <fredyang@saas.top>
2021-05-08 17:56:36 +0800

    init--先把项目跑起来

--- yeyue-maven-club/src/main/java/club/yeyue/maven/YeyueMavenClubApplication.java
+++ yeyue-maven-club/src/main/java/club/yeyue/maven/YeyueMavenClubApplication.java
@@ -1,13 +1,31 @@
 package club.yeyue.maven;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+/**
+ * 启动类
+ *
+ * @author fred
+ * @date 2021-05-08 15:49
+ */
+@Slf4j
 @SpringBootApplication
-public class YeyueMavenClubApplication {
+public class YeyueMavenClubApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(YeyueMavenClubApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        new SpringApplicationBuilder(YeyueMavenClubApplication.class)
+                .web(WebApplicationType.NONE).build().run(args);
+        synchronized (YeyueMavenClubApplication.class) {
+            YeyueMavenClubApplication.class.wait();
+        }
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("系统已经跑起来了冲冲冲---->>>>>");
+    }
 }
