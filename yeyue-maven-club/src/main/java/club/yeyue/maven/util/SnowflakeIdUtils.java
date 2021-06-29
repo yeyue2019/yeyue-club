d032da14432fd85e6bedbd2f119b598231982e93
fred <fredyang@saas.top>
2021-06-29 14:39:11 +0800

    update--添加一个加解密工具类

--- yeyue-maven-club/src/main/java/club/yeyue/maven/util/SnowflakeIdUtils.java
+++ yeyue-maven-club/src/main/java/club/yeyue/maven/util/SnowflakeIdUtils.java
@@ -14,114 +14,52 @@ public class SnowflakeIdUtils {
     private SnowflakeIdUtils() {
     }
 
-    public SnowflakeIdUtils(long datacenterId, long workerId) {
-        this.datacenterId = datacenterId;
-        this.workerId = workerId;
-    }
-
     /**
      * 时间起始标记点，作为基准，一般取系统的最近时间（一旦确定不能变动）
      */
     private static final long TWEPOCH = 1288834974657L;
-    /**
-     * 机器标识位数
-     */
-    private static final long WORKER_ID_BITS = 5L;
-    /**
-     * 数据中心标识位数
-     */
-    private static final long DATA_CENTER_ID_BITS = 5L;
-    /**
-     * 机器ID最大值
-     */
-    private static final long MAX_WORKER_ID = ~(-1L << WORKER_ID_BITS);
-    /**
-     * 数据中心ID最大值
-     */
-    private static final long MAX_DATA_CENTER_ID = ~(-1L << DATA_CENTER_ID_BITS);
-    /**
-     * 毫秒内自增位
-     */
-    public static final long SEQUENCE_BITS = 12L;
-    /**
-     * 机器ID偏左移12位
-     */
-    public static final long WORKER_ID_SHIFT = SEQUENCE_BITS;
-    /**
-     * 数据中心ID左移17位
-     */
-    public static final long DATA_CENTER_ID_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS;
-    /**
-     * 时间毫秒左移22位
-     */
-    public static final long TIMESTAMP_SHIFT = SEQUENCE_BITS + WORKER_ID_BITS + DATA_CENTER_ID_BITS;
-    /**
-     * 自增位最大值
-     */
-    private static final long SEQUENCE_MASK = ~(-1L << SEQUENCE_BITS);
-    /**
-     * 上次生产Id的时间戳
-     */
-    private long lastTimeStamp = -1L;
+
     /**
      * 并发控制
      */
-    private long sequence = 0L;
+    private static Long SEQUENCE = 0L;
 
     /**
-     * 工作机器0～31
+     * 时间戳
      */
-    private long datacenterId;
-    /**
-     * 数据中心Id0～31
-     */
-    private long workerId;
-
-    private static final SnowflakeIdUtils INSTANCE;
-
-    static {
-        INSTANCE = new SnowflakeIdUtils();
-        INSTANCE.datacenterId = 0L;
-        INSTANCE.workerId = 0L;
-    }
-
+    private static Long LAST_TIME_STAMP = -1L;
 
     /**
      * 获取下一个Id
      *
+     * @param datacenterId 数据中心编码 0-31
+     * @param workerId     机器编码 0-31
      * @return 结果
      */
-    private synchronized long nextId() {
-        datacenterId = datacenterId % (MAX_DATA_CENTER_ID + 1);
-        workerId = workerId % (MAX_WORKER_ID + 1);
+    public static synchronized long generate(long datacenterId, long workerId) {
+        datacenterId = datacenterId % 32;
+        workerId = workerId % 32;
         long timestamp = System.currentTimeMillis();
-        if (timestamp < lastTimeStamp) {
-            throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimeStamp - timestamp));
+        if (timestamp < LAST_TIME_STAMP) {
+            throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", LAST_TIME_STAMP - timestamp));
         }
-        if (lastTimeStamp == timestamp) {
+        if (LAST_TIME_STAMP == timestamp) {
             // 当前毫秒内，则+1
-            sequence = (sequence + 1) & SEQUENCE_MASK;
-            if (sequence == 0) {
+            SEQUENCE = ((SEQUENCE + 1L) & 4095);
+            if (SEQUENCE == 0) {
                 // 当前毫秒内计数满了，则等待下一秒
                 do {
                     timestamp = System.currentTimeMillis();
-                } while (timestamp <= lastTimeStamp);
+                } while (timestamp <= LAST_TIME_STAMP);
             }
         } else {
-            sequence = 0L;
+            SEQUENCE = 0L;
         }
-        lastTimeStamp = timestamp;
+        LAST_TIME_STAMP = timestamp;
         // ID偏移组合生成最终的ID，并返回ID
-        return ((timestamp - TWEPOCH) << TIMESTAMP_SHIFT)
-                | (datacenterId << DATA_CENTER_ID_SHIFT)
-                | (workerId << WORKER_ID_SHIFT) | sequence;
+        return ((timestamp - TWEPOCH) << 22)
+                | (datacenterId << 17)
+                | (workerId << 12) | SEQUENCE;
     }
 
-    public static long generate() {
-        return INSTANCE.nextId();
-    }
-
-    public static long generate(SnowflakeIdUtils instance) {
-        return instance.nextId();
-    }
 }
